cee1c17af172785af0fc80c175ccedec9ec48625
lhf93 <347784700@qq.com>
2018-07-18 19:08:31 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Diary from '@/components/Diary'
+import Publish from '@/components/Publish'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +11,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/diary',
+      name: 'Diary',
+      component: Diary
+    },
+    {
+      path: '/publish',
+      name: 'Publish',
+      component: Publish
     }
   ]
 })
