b0ac87ccfbdd22eb18a3e5d4c8397df4901b8747
doulinxu <347247864@qq.com>
2018-07-20 15:16:33 +0800

    新增公共弹窗组件 依赖增加less

--- src/main.js
+++ src/main.js
@@ -8,7 +8,8 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-// import './assets/styles/reset.css'
+// import 'styles/reset.css'
+// import 'styles/iconfont.css'
 // import './assets/styles/iconfont.css'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
