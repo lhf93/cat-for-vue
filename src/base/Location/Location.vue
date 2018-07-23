83a828670761d5fffe4ac66245a65482cd6ff9ad
hfj <fujian.hou@bestmath.cn>
2018-07-23 13:54:45 +0800

    刪除无用注释

--- src/base/Location/Location.vue
+++ src/base/Location/Location.vue
@@ -21,20 +21,13 @@ export default {
 </script>
 <style lang='stylus' scoped>
   .location
-    // width: 500px
     display flex
     align-items center
     height: 50px
     line-height: 50px
-    // border: 1px solid #ccc
-    // border-radius: 2px
     font-size: 14px
     color: #747474
-    // vertical-align: middle
     .icon-l
       margin-right: 10px
       cursor: pointer
-    // span
-      // position: relative
-      // top: -30%
 </style>
